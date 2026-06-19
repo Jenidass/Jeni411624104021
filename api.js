d1d4616b3d869fb863991ae4812372c710323b68
Jenidass <jenidass1007@gmail.com>
2026-06-19 12:05:22 +0530

    Rest api and task

---
+++ api.js
@@ -0,0 +1,14 @@
+fetch("https://jsonplaceholder.typicode.com/posts/5",
+    {
+        method: "PATCH",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            title: 'New title',
+           
+        })
+    })
+
+.then(response=>response.json())
+.then(data=>{console.log(data)})
